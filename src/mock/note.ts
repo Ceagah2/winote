abdedee59004171a48550ea8ff3274aa03e64913
Carlos Ceagah <carlosceagah@gmail.com>
2024-06-08 13:04:24 -0300

    feat: create mocked file to finish layout

---
+++ src/mock/note.ts
@@ -0,0 +1,23 @@
+export const MockedNote = {
+  id: 1,
+  title: "Aula 01 - vinhos tintos",
+  description: "Aula de vinhos tintos",
+  photos: [
+    {
+      id: 1,
+      uri: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
+    },
+    {
+      id: 2,
+      uri: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
+    },
+    {
+      id: 3,
+      uri: "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg",
+    },
+  ],
+  audios: null,
+  createdAt: "2022-08-17T20:01:15.000Z",
+  updatedAt: "2022-08-17T20:01:15.000Z",
+
+}
